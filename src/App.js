f2ad5560d0e1b3741ba9db747005b441e516a551
CarlClayton <carlclayton1984@gmail.com>
2020-03-09 22:23:30 +0000

    build page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './header';
+import FAQ from './FAQ';
 
 function App() {
+  const [faqs, setfaqs] = useState([
+    {
+      question: 'How does React work?',
+      answer: 'magic wizards',
+      open: true
+    },
+    {
+      question: 'How does HTML work?',
+      answer: 'witch craft',
+      open: false
+    },
+    {
+      question: 'How does JavaScript work?',
+      answer: 'unicorn glitter',
+      open: false
+    }
+  ]);
+
+  const toggleFAQ = index => {
+    setfaqs(faqs.map((faq, i) => {
+      if (i === index) {
+        faq.open = !faq.open
+      } else {
+        faq.open = false;
+      }
+
+      return faq;
+    }))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <div className='faqs'>
+        {faqs.map((faq, i) => (
+          <FAQ faq={faq} index={i} toggleFAQ={toggleFAQ}/>
+        ))}
+      </div>
     </div>
   );
 }
